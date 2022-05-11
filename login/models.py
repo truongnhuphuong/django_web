cb94f390ac1a16a3b5d702852ada27128e42b1a5
Nhu Phuong <nhuphuongau2019@gmail.com>
2022-05-11 10:51:40 +0900

    add

---
+++ login/models.py
@@ -0,0 +1,11 @@
+from pyexpat import model
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.username
+
